420e0215921788b74a6152c151e9f19fcab48c8d
Photon <novus.futura@gmail.com>
2021-01-12 00:48:37 +0800

    Parse list into about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,42 @@
 <template>
   <h1>About Page</h1>
+  <div class="cdiv">
+  <h4>All cards - Categories</h4>
+  <ul>
+    <li class="all_lst " v-for="(item,index) in stkLst" :key="item">
+    {{ index }}
+        <ul>
+          <li v-for="i in item" :key="i">
+         {{ i  }}
+       </li>
+     </ul>
+    </li>
+  </ul>
+</div>
 </template>
+
+
+<script>
+import sList from "@/dat/stocklist.json";
+
+export default {
+  data() {
+    return{
+      stkLst: sList,
+    }
+  }
+
+}
+</script>
+
+
+<style>
+.cdiv{
+  margin:0px auto;
+        display: table;
+}
+
+.all_lst{
+  text-align: left;
+}
+</style>
